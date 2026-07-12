9d4302a4ef701497582b9c74379dfcfff412da21
dtc225200690-sudo <dtc225200690@ictu.edu.vn>
2026-07-12 19:21:23 +0700

    Add Google Analytics tracking

---
+++ assets/js/analytics.js
@@ -0,0 +1,15 @@
+(function () {
+  var measurementId = "G-XT4BNSPHYE";
+  var script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(measurementId);
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+
+  gtag("js", new Date());
+  gtag("config", measurementId);
+})();
